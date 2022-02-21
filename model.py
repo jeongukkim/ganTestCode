fa00593478591c5e77779975eee4f7e9addd658f
JeongUk <kju657573@gmail.com>
2022-02-21 23:09:09 +0900

    refactor codes..

--- model.py
+++ model.py
@@ -15,7 +15,16 @@ RES_TO_CHANNELS = {
     128: 128,
     256: 64,
     512: 32,
-    1024: 16
+    1024: 16,
+}
+SIZE_TO_BLUR_KERNEL = {
+    1: [1],
+    2: [1, 1],
+    3: [1, 2, 1],
+    4: [1, 3, 3, 1],
+    5: [1, 4, 6, 4, 1],
+    6: [1, 5, 10, 10, 5, 1],
+    7: [1, 6, 15, 20, 15, 6, 1],
 }
 
 
@@ -63,19 +72,19 @@ class MappingNetwork(nn.Module):
         return self._numLayers
 
     def forward(self, latent):
-        # latent = latent * latent.square().mean(dim=1, keepdim=True).add_(EPSILON).rsqrt()
-        latent = latent * (latent.square().mean(dim=1, keepdim=True)+EPSILON).rsqrt()
+        latent = latent * latent.square().mean(dim=1, keepdim=True).add_(EPSILON).rsqrt()
         intermediateLatent = self.fcs(latent)
 
         return intermediateLatent
 
 
 class LinearLayer(nn.Module):
-    """ Fully Connected Layer + Leaky ReLU (alpha=0.2)"""
+    """ Fully Connected Layer (+ Leaky ReLU with the negative slope of 0.2)"""
 
-    def __init__(self, inChannels, outChannels, lrMultiplier=1.):
+    def __init__(self, inChannels, outChannels, lrMultiplier=1., doActivation=True):
         super(LinearLayer, self).__init__()
 
+        self._doActivation = doActivation
         self._lrMultiplier = lrMultiplier
 
         initNormalDist = torch.randn((outChannels, inChannels)) / lrMultiplier
@@ -84,18 +93,20 @@ class LinearLayer(nn.Module):
         self.bias = nn.Parameter(initZeros)
 
         # Equalized Learning Rate
-        # :: times sqrt(2/fan_in)
+        # :: times sqrt(2/fan_in) for ReLU-type activation
         self._weightMultiplier = np.sqrt(1. / inChannels)
 
-    def forward(self, latent, lrelu=True):
+    def forward(self, latent):
         w = self.weight * self._weightMultiplier * self._lrMultiplier
         b = self.bias.unsqueeze(dim=0) * self._lrMultiplier
 
         latent = F.linear(latent, weight=w)
-        if lrelu:
+
+        if self._doActivation:
+            # to retain expected signal variance, multiply sqrt(2)
             latent = F.leaky_relu(latent.add_(b), negative_slope=0.2) * np.sqrt(2)
         else:
-            latent.add_(b)
+            latent = latent.add_(b)
 
         return latent
 
@@ -149,7 +160,7 @@ class SynthesisNetwork(nn.Module):
 
 
 class SynthesisBlock(nn.Module):
-    """ Network block where ONE style is active """
+    """ Network block where ONE style is active + applying noise and bias"""
 
     def __init__(self, latentDim, inChannels, outChannels, kernelSize, doUpsample=False):
         super(SynthesisBlock, self).__init__()
@@ -162,7 +173,7 @@ class SynthesisBlock(nn.Module):
         self.noiseWeight = nn.Parameter(initZero)
 
     def forward(self, content, latent):
-        # Fused Convolution
+        # Style Block
         content = self.conv(content, latent)
 
         # NoiseInjection
@@ -171,6 +182,7 @@ class SynthesisBlock(nn.Module):
         content = content.add_(self.noiseWeight * noise)
 
         # AddBias -> Activation
+        # to retain expected signal variance, multiply sqrt(2)
         content = F.leaky_relu(content.add_(self.bias), negative_slope=0.2) * np.sqrt(2)
 
         return content
@@ -179,7 +191,7 @@ class SynthesisBlock(nn.Module):
 class ConvLayer(nn.Module):
     """ Convolution Layer in StyleGAN2 """
 
-    def __init__(self, latentDim, inChannels, outChannels, kernelSize, doUpsample=False, doDownsample=False, doDemod=True):
+    def __init__(self, latentDim, inChannels, outChannels, kernelSize, filterSize=4, doUpsample=False, doDownsample=False, doDemod=True):
         super(ConvLayer, self).__init__()
 
         assert doUpsample * doDownsample == 0
@@ -187,85 +199,94 @@ class ConvLayer(nn.Module):
         self._doDownsample = doDownsample
         self._doDemod = doDemod
 
-        self.affine = LinearLayer(latentDim, inChannels)
+        self.affine = LinearLayer(latentDim, inChannels, doActivation=False)
         self.affine.bias.data.fill_(1)
 
-        h, w = [kernelSize] * 2
-        initNormalDist = torch.randn((outChannels, inChannels, h, w))
+        kh, kw = [kernelSize] * 2
+        initNormalDist = torch.randn((outChannels, inChannels, kh, kw))
         self.weight = nn.Parameter(initNormalDist)
 
         # padding
         self._padding = kernelSize // 2
 
         # blur filter
-        self._filter = [1, 3, 3, 1]
-
-        # Equalized Learning Rate
-        # :: times sqrt(2/fan_in)
-        fanIn = inChannels * h * w
-        self._weightMultiplier = np.sqrt(1. / fanIn)
-
-    def get_filter(self):
-        filter = torch.tensor(self._filter)
+        # NOT parameter, just state
+        filter = SIZE_TO_BLUR_KERNEL[filterSize]
+        filter = torch.tensor(filter)
         filter = filter.outer(filter)
         filter = filter / filter.sum()
+        self.register_buffer("filter", filter)
 
-        return filter
+        # Equalized Learning Rate
+        # :: times sqrt(2/fan_in) for ReLU-type activation
+        fanIn = inChannels * kh * kw
+        self._weightMultiplier = np.sqrt(1. / fanIn)
 
-    def forward(self, inTensor, latent, styleMultiplier=1.):
-        # Bilinear filtering if needed
-        # if self._doUpsample:
-        #     inTensor = F.interpolate(inTensor, None, 2, 'bilinear')
-        # if self._doDownsample:
-        #     inTensor = F.interpolate(inTensor, None, 0.5, 'bilinear')
+    def forward(self, inTensor, latent):
+        weight = self._normalizeWeight(latent)
+        outTensor = self._convMain(inTensor, weight)
 
-        batchSize, inChannels, imgHeight, imgWidth = inTensor.shape
+        return outTensor
 
-        # Affine transformation
-        style = self.affine(latent, lrelu=False) * styleMultiplier
+    def _normalizeWeight(self, latent):
+        styleMultiplier = self._weightMultiplier if not self._doDemod else 1
 
-        # Equalized LR
-        # weight = self.weight * self._weightMultiplier
+        style = self.affine(latent) * styleMultiplier
         weight = self.weight
 
         # Reshape to [batchSize, outChannels, inChannels, kernelSize, kernelSize]
-        style = style.reshape(batchSize, 1, -1, 1, 1)
+        style = style.reshape(-1, 1, weight.size(dim=1), 1, 1)
         weight = weight.unsqueeze(0)
 
         # Modulation
         weight = weight * style
 
-        # Demodulation if needed
+        # Demodulation
         if self._doDemod:
-            # norm = weight.square().sum(dim=[2, 3, 4], keepdim=True).add_(1e-8).rsqrt()
-            norm = (weight.square().sum(dim=[2, 3, 4], keepdim=True)+1e-8).rsqrt()
+            norm = weight.square().sum(dim=[2, 3, 4], keepdim=True).add_(1e-8).rsqrt()
             weight = weight * norm
 
-        # Reshape and Group Convolution
+        return weight
+
+    def _convMain(self, inTensor, weight):
+        batchSize, _, imgHeight, imgWidth = inTensor.shape
+
+        # Group Convolution sees one sample with N groups
         inTensor = inTensor.reshape(1, -1, imgHeight, imgWidth)
         if self._doUpsample:
-            weight = weight.transpose(1, 2)
-            weight = weight.reshape(-1, *weight.shape[2:])
-
-            outTensor = F.conv_transpose2d(inTensor, weight=weight, stride=2, padding=(0, 0), groups=batchSize)
-
-            outTensor = F.pad(outTensor, [1, 1, 1, 1])
-
-            channels = outTensor.size(dim=1)
-
-            blur_filter = self.get_filter()
-            blur_filter *= 4
-            blur_filter = blur_filter[None, None, :, :].repeat(channels, 1, 1, 1)
-            blur_filter = blur_filter.to(outTensor.device)
-
-            outTensor = F.conv2d(outTensor, weight=blur_filter, groups=channels)
-
+            outTensor = self._transeposeStridedConvThenBlur(inTensor, weight, numGroups=batchSize)
         else:
-            weight = weight.reshape(-1, *weight.shape[2:])
-
-            outTensor = F.conv2d(inTensor, weight=weight, padding=self._padding, groups=batchSize)
+            outTensor = self._plainConv(inTensor, weight, numGroups=batchSize)
 
         outTensor = outTensor.reshape(batchSize, -1, *outTensor.shape[2:])
+
+        return outTensor
+
+    def _transeposeStridedConvThenBlur(self, inTensor, weight, numGroups):
+        # transepose strided group convolution
+        weight = weight.transpose(1, 2)
+        weight = weight.reshape(-1, *weight.shape[2:])
+
+        outTensor = F.conv_transpose2d(inTensor, weight=weight, stride=2, padding=(0, 0), groups=numGroups)
+
+        # Blur
+        outTensor = F.pad(outTensor, [1, 1, 1, 1])
+
+        numChannels = outTensor.size(dim=1)
+
+        blurFilter = self.filter
+        blurFilter *= 4
+        blurFilter = blurFilter[None, None, :, :].repeat(numChannels, 1, 1, 1)
+        blurFilter = blurFilter.to(outTensor.device)
+
+        outTensor = F.conv2d(outTensor, weight=blurFilter, groups=numChannels)
+
+        return outTensor
+
+    def _plainConv(self, inTensor, weight, numGroups):
+        weight = weight.reshape(-1, *weight.shape[2:])
+
+        outTensor = F.conv2d(inTensor, weight=weight, padding=self._padding, groups=numGroups)
 
         return outTensor
 
@@ -273,35 +294,44 @@ class ConvLayer(nn.Module):
 class ToRgb(nn.Module):
     """ Convert to RGB using output skips"""
 
-    def __init__(self, latentDim, inChannels):
+    def __init__(self, latentDim, inChannels, filterSize=4):
         super(ToRgb, self).__init__()
 
         self.conv = ConvLayer(latentDim, inChannels, outChannels=3, kernelSize=1, doDemod=False)
         initZeros = torch.zeros((1, 3, 1, 1))
         self.bias = nn.Parameter(initZeros)
 
-        fanIn = inChannels
-        self._multiplier = np.sqrt(1. / fanIn)
+        # blur filter
+        # NOT parameter, just state
+        filter = SIZE_TO_BLUR_KERNEL[filterSize]
+        filter = torch.tensor(filter)
+        filter = filter.outer(filter)
+        filter = filter / filter.sum()
+        self.register_buffer("filter", filter)
 
     def forward(self, prevTensor, inTensor, latent):
-        outTensor = self.conv(inTensor, latent, styleMultiplier=self._multiplier).add_(self.bias)
-
-        if prevTensor is not None:
-            batchSize, inChannel, imageHeight, imageWidth = prevTensor.shape
-            prevTensor = prevTensor.reshape(batchSize, inChannel, imageHeight, 1, imageWidth, 1)
-            prevTensor = F.pad(prevTensor, [0, 1, 0, 0, 0, 1])
-            prevTensor = prevTensor.reshape(batchSize, inChannel, imageHeight * 2, imageWidth * 2)
-            prevTensor = F.pad(prevTensor, [2, 1, 2, 1])
-
-            filter = torch.tensor([1,3,3,1])
-            filter = filter.outer(filter)
-            filter = filter / filter.sum()
-            filter *= 4
-            filter = filter[None, None, :, :].repeat(inChannel, 1, 1, 1)
-            filter = filter.to(outTensor.device)
-
-            prevTensor = F.conv2d(prevTensor, weight=filter, groups=inChannel)
-
-            outTensor += prevTensor
+        outTensor = self.conv(inTensor, latent).add_(self.bias)
+        outTensor = self._skipConnectionAfterUpsample(prevTensor, outTensor)
 
         return outTensor
+
+    def _skipConnectionAfterUpsample(self, prevImg, outputImg):
+        if prevImg is not None:
+            # Upsampling
+            batchSize, inChannel, imageHeight, imageWidth = prevImg.shape
+            prevImg = prevImg.reshape(batchSize, inChannel, imageHeight, 1, imageWidth, 1)
+            prevImg = F.pad(prevImg, [0, 1, 0, 0, 0, 1])
+            prevImg = prevImg.reshape(batchSize, inChannel, imageHeight * 2, imageWidth * 2)
+            prevImg = F.pad(prevImg, [2, 1, 2, 1])
+
+            blurFilter = self.filter
+            blurFilter *= 4
+            blurFilter = blurFilter[None, None, :, :].repeat(inChannel, 1, 1, 1)
+            blurFilter = blurFilter.to(outputImg.device)
+
+            prevImg = F.conv2d(prevImg, weight=blurFilter, groups=inChannel)
+
+            # Summing
+            outputImg += prevImg
+
+        return outputImg
